fb5976db76c5a8f9faf0398e593938a43c193330
Wang Han <416810799@qq.com>
2017-05-05 07:09:53 -0700

    PM / suspend: Introduce Oneplus 3 fingerprintd thaw hack
    Taken from Oneplus 3, this hack will make fingerprintd recovery
    from suspend quickly.
    
    [Tashar02: Use strstr() instead of memcmp() and search for "fingerprint"]
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -388,6 +388,8 @@ void __weak arch_suspend_enable_irqs(void)
 	local_irq_enable();
 }
 
+extern void thaw_fingerprintd(void);
+
 /**
  * suspend_enter - Make the system enter the given sleep state.
  * @state: System sleep state to enter.
@@ -472,6 +474,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	suspend_enable_secondary_cpus();
 
  Platform_wake:
+	thaw_fingerprintd();
 	platform_resume_noirq(state);
 	dpm_resume_noirq(PMSG_RESUME);
 
