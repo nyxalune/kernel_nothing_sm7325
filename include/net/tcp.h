47fcd75adbffb7c58768baead14635778e7efc33
Neal Cardwell <ncardwell@google.com>
2018-08-07 21:52:06 -0400

    net-tcp_bbr: v2: introduce ca_ops->skb_marked_lost() CC module callback API
    For connections experiencing reordering, RACK can mark packets lost
    long after we receive the SACKs/ACKs hinting that the packets were
    actually lost.
    
    This means that CC modules cannot easily learn the volume of inflight
    data at which packet loss happens by looking at the current inflight
    or even the packets in flight when the most recently SACKed packet was
    sent. To learn this, CC modules need to know how many packets were in
    flight at the time lost packets were sent. This new callback, combined
    with TCP_SKB_CB(skb)->tx.in_flight, allows them to learn this.
    
    This also provides a consistent callback that is invoked whether
    packets are marked lost upon ACK processing, using the RACK reordering
    timer, or at RTO time.
    
    Effort: net-tcp_bbr
    Origin-9xx-SHA1: afcbebe3374e4632ac6714d39e4dc8a8455956f4
    Change-Id: I54826ab53df636be537e5d3c618a46145d12d51a
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -14,7 +14,7 @@
 #ifndef _TCP_H
 #define _TCP_H
 
-#define FASTRETRANS_DEBUG 0
+#define FASTRETRANS_DEBUG 1
 
 #include <linux/list.h>
 #include <linux/tcp.h>
@@ -23,6 +23,7 @@
 #include <linux/cache.h>
 #include <linux/percpu.h>
 #include <linux/skbuff.h>
+#include <linux/cryptohash.h>
 #include <linux/kref.h>
 #include <linux/ktime.h>
 
@@ -588,8 +589,6 @@ __u32 cookie_v6_init_sequence(const struct sk_buff *skb, __u16 *mss);
 #endif
 /* tcp_output.c */
 
-u32 tcp_tso_autosize(const struct sock *sk, unsigned int mss_now,
-		     int min_tso_segs);
 void __tcp_push_pending_frames(struct sock *sk, unsigned int cur_mss,
 			       int nonagle);
 int __tcp_retransmit_skb(struct sock *sk, struct sk_buff *skb, int segs);
@@ -1113,8 +1112,8 @@ struct tcp_congestion_ops {
 	void (*pkts_acked)(struct sock *sk, const struct ack_sample *sample);
 	/* override sysctl_tcp_min_tso_segs */
 	u32 (*min_tso_segs)(struct sock *sk);
-	/* suggest number of segments for each skb to transmit (optional) */
-	u32 (*tso_segs_goal)(struct sock *sk);
+	/* react to a specific lost skb (optional) */
+	void (*skb_marked_lost)(struct sock *sk, const struct sk_buff *skb);
 	/* returns the multiplier used in tcp_sndbuf_expand (optional) */
 	u32 (*sndbuf_expand)(struct sock *sk);
 	/* call when packets are delivered to update cwnd and pacing rate,
@@ -1334,19 +1333,10 @@ static inline bool tcp_is_cwnd_limited(const struct sock *sk)
  * but is not always installed/used.
  * Return true if TCP stack should pace packets itself.
  */
-#if defined(CONFIG_DEFAULT_BBR) && !defined(CONFIG_DEFAULT_FQ)
-// FORCE ENABLE TCP INTERNAL PACING with default BBR without FQ
-static inline bool tcp_needs_internal_pacing(struct sock *sk)
-{
-	sk->sk_pacing_status = SK_PACING_NEEDED;
-	return true;
-}
-#else
 static inline bool tcp_needs_internal_pacing(const struct sock *sk)
 {
 	return smp_load_acquire(&sk->sk_pacing_status) == SK_PACING_NEEDED;
 }
-#endif
 
 /* Return in jiffies the delay before one skb is sent.
  * If @skb is NULL, we look at EDT for next packet being sent on the socket.
