f075d21fae0cded54906fcfc744b1e2ffc01070d
Jason A. Donenfeld <Jason@zx2c4.com>
2019-11-25 11:31:12 +0100

    crypto: arch - conditionalize crypto api in arch glue for lib code
    commit 8394bfec51e0e565556101bcc4e2fe7551104cd8 upstream.
    
    For glue code that's used by Zinc, the actual Crypto API functions might
    not necessarily exist, and don't need to exist either. Before this
    patch, there are valid build configurations that lead to a unbuildable
    kernel. This fixes it to conditionalize those symbols on the existence
    of the proper config entry.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Change-Id: I514def4191a6782966b948c21aadd1cd02a85b3e
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Change-Id: I514def4191a6782966b948c21aadd1cd02a85b3e
    (cherry picked from commit 4d580570f81e6aee2a75167f39bf6c93f976a752)
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- arch/x86/crypto/poly1305_glue.c
+++ arch/x86/crypto/poly1305_glue.c
@@ -217,7 +217,7 @@ static int __init poly1305_simd_mod_init(void)
 	    cpu_has_xfeatures(XFEATURE_MASK_SSE | XFEATURE_MASK_YMM, NULL))
 		static_branch_enable(&poly1305_use_avx2);
 
-	return crypto_register_shash(&alg);
+	return IS_REACHABLE(CONFIG_CRYPTO_HASH) ? crypto_register_shash(&alg) : 0;
 }
 
 static void __exit poly1305_simd_mod_exit(void)
