bee5ec2618b1063a7e45296d2264a85ba627d59a
Qian Cai <cai@lca.pw>
2020-02-19 11:00:54 -0800

    BACKPORT: fork: Annotate a data race in vm_area_dup()
    struct vm_area_struct could be accessed concurrently as noticed by
    KCSAN,
    
     write to 0xffff9cf8bba08ad8 of 8 bytes by task 14263 on cpu 35:
      vma_interval_tree_insert+0x101/0x150:
      rb_insert_augmented_cached at include/linux/rbtree_augmented.h:58
      (inlined by) vma_interval_tree_insert at mm/interval_tree.c:23
      __vma_link_file+0x6e/0xe0
      __vma_link_file at mm/mmap.c:629
      vma_link+0xa2/0x120
      mmap_region+0x753/0xb90
      do_mmap+0x45c/0x710
      vm_mmap_pgoff+0xc0/0x130
      ksys_mmap_pgoff+0x1d1/0x300
      __x64_sys_mmap+0x33/0x40
      do_syscall_64+0x91/0xc44
      entry_SYSCALL_64_after_hwframe+0x49/0xbe
    
     read to 0xffff9cf8bba08a80 of 200 bytes by task 14262 on cpu 122:
      vm_area_dup+0x6a/0xe0
      vm_area_dup at kernel/fork.c:362
      __split_vma+0x72/0x2a0
      __split_vma at mm/mmap.c:2661
      split_vma+0x5a/0x80
      mprotect_fixup+0x368/0x3f0
      do_mprotect_pkey+0x263/0x420
      __x64_sys_mprotect+0x51/0x70
      do_syscall_64+0x91/0xc44
      entry_SYSCALL_64_after_hwframe+0x49/0xbe
    
    vm_area_dup() blindly copies all fields of original VMA to the new one.
    This includes coping vm_area_struct::shared.rb which is normally
    protected by i_mmap_lock. But this is fine because the read value will
    be overwritten on the following __vma_link_file() under proper
    protection. Thus, mark it as an intentional data race and insert a few
    assertions for the fields that should not be modified concurrently.
    
    Signed-off-by: Qian Cai <cai@lca.pw>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    [Tashar02: We don't care about assertions since KCSAN isn't available on msm-5.4]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -361,7 +361,11 @@ struct vm_area_struct *vm_area_dup(struct vm_area_struct *orig)
 	struct vm_area_struct *new = kmem_cache_alloc(vm_area_cachep, GFP_KERNEL);
 
 	if (new) {
-		*new = *orig;
+		/*
+		 * orig->shared.rb may be modified concurrently, but the clone
+		 * will be reinitialized.
+		 */
+		*new = data_race(*orig);
 		INIT_LIST_HEAD(&new->anon_vma_chain);
 	}
 	return new;
