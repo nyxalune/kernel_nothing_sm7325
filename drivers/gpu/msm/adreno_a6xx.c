9780f5cf153d9d2a13c35c44ecddd4a962ab94cc
Juhyung Park <qkrwngud825@gmail.com>
2021-09-17 21:34:36 +0900

    adreno: disable snapshot, coresight and trace
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_a6xx.c
+++ drivers/gpu/msm/adreno_a6xx.c
@@ -1038,9 +1038,6 @@ void a6xx_spin_idle_debug(struct adreno_device *adreno_dev,
 		adreno_dev->cur_rb->id, rptr, wptr, status, status3, intstatus);
 
 	dev_err(device->dev, " hwfault=%8.8X\n", hwfault);
-
-	kgsl_device_snapshot(device, NULL, false);
-
 }
 
 /*
@@ -2608,7 +2605,6 @@ const struct adreno_gpudev adreno_a6xx_gpudev = {
 	.reg_offsets = a6xx_register_offsets,
 	.probe = a6xx_probe,
 	.start = a6xx_start,
-	.snapshot = a6xx_snapshot,
 	.init = a6xx_init,
 	.irq_handler = a6xx_irq_handler,
 	.rb_start = a6xx_rb_start,
@@ -2625,9 +2621,6 @@ const struct adreno_gpudev adreno_a6xx_gpudev = {
 	.set_marker = a6xx_set_marker,
 	.preemption_context_init = a6xx_preemption_context_init,
 	.ccu_invalidate = a6xx_ccu_invalidate,
-#ifdef CONFIG_QCOM_KGSL_CORESIGHT
-	.coresight = {&a6xx_coresight, &a6xx_coresight_cx},
-#endif
 	.read_alwayson = a6xx_read_alwayson,
 	.power_ops = &adreno_power_operations,
 	.clear_pending_transactions = a6xx_clear_pending_transactions,
@@ -2637,15 +2630,11 @@ const struct adreno_gpudev adreno_a6xx_gpudev = {
 const struct adreno_gpudev adreno_a6xx_hwsched_gpudev = {
 	.reg_offsets = a6xx_register_offsets,
 	.probe = a6xx_hwsched_probe,
-	.snapshot = a6xx_hwsched_snapshot,
 	.irq_handler = a6xx_irq_handler,
 	.read_throttling_counters = a6xx_read_throttling_counters,
 	.iommu_fault_block = a6xx_iommu_fault_block,
 	.preemption_context_init = a6xx_preemption_context_init,
 	.context_detach = a6xx_hwsched_context_detach,
-#ifdef CONFIG_QCOM_KGSL_CORESIGHT
-	.coresight = {&a6xx_coresight, &a6xx_coresight_cx},
-#endif
 	.read_alwayson = a6xx_read_alwayson,
 	.power_ops = &a6xx_hwsched_power_ops,
 };
@@ -2654,7 +2643,6 @@ const struct adreno_gpudev adreno_a6xx_gmu_gpudev = {
 	.reg_offsets = a6xx_register_offsets,
 	.probe = a6xx_gmu_device_probe,
 	.start = a6xx_start,
-	.snapshot = a6xx_gmu_snapshot,
 	.init = a6xx_init,
 	.irq_handler = a6xx_irq_handler,
 	.rb_start = a6xx_rb_start,
@@ -2685,7 +2673,6 @@ const struct adreno_gpudev adreno_a6xx_rgmu_gpudev = {
 	.reg_offsets = a6xx_register_offsets,
 	.probe = a6xx_rgmu_device_probe,
 	.start = a6xx_start,
-	.snapshot = a6xx_rgmu_snapshot,
 	.init = a6xx_init,
 	.irq_handler = a6xx_irq_handler,
 	.rb_start = a6xx_rb_start,
@@ -2713,7 +2700,6 @@ const struct adreno_gpudev adreno_a619_holi_gpudev = {
 	.reg_offsets = a6xx_register_offsets,
 	.probe = a6xx_probe,
 	.start = a6xx_start,
-	.snapshot = a6xx_snapshot,
 	.init = a6xx_holi_init,
 	.irq_handler = a6xx_irq_handler,
 	.rb_start = a6xx_rb_start,
@@ -2747,7 +2733,6 @@ const struct adreno_gpudev adreno_a630_gpudev = {
 	.reg_offsets = a6xx_register_offsets,
 	.probe = a6xx_gmu_device_probe,
 	.start = a6xx_start,
-	.snapshot = a6xx_gmu_snapshot,
 	.init = a6xx_init,
 	.irq_handler = a6xx_irq_handler,
 	.rb_start = a6xx_rb_start,
