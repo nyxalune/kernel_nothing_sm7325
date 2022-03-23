5f18d5469f32954f1d70c0b4546798f235c3a63a
Madhu Ananthula <quic_mananthu@quicinc.com>
2022-03-23 14:42:58 +0530

    drivers: devfreq: Fix possible divide-by-zero
    Fix possible divide-by-zero in update_counts
    function in arm_memlat_mon driver.
    
    Signed-off-by: Madhu Ananthula <quic_mananthu@quicinc.com>
    Signed-off-by: Shreyas K K <quic_shrekk@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -205,7 +205,10 @@ static void update_counts(struct memlat_cpu_grp *cpu_grp)
 			cpu_grp->read_event_cpu = -1;
 		}
 
-		cpu_data->freq = common_evs[CYC_IDX].last_delta / delta;
+		if (delta != 0)
+			cpu_data->freq = common_evs[CYC_IDX].last_delta / delta;
+		else
+			cpu_data->freq = common_evs[CYC_IDX].last_delta;
 
 		if (!common_evs[STALL_IDX].pevent)
 			common_evs[STALL_IDX].last_delta =
