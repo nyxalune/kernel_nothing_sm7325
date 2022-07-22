24c5a3bdf3359a42503e64c275eb20a31f51bfb9
Sushmita Susheelendra <quic_ssusheel@quicinc.com>
2022-07-21 22:15:58 -0400

    msm: kgsl: Fix command completion race with profile IB write
    Ensure that the submit data for user profiling is written
    out to the profiling IB before we submit work to the GMU
    dispatch queue. Otherwise, if the GMU is ready to dispatch
    and the GPU work is small, it can so happen that the command
    retires even before the user profiling data gets written out
    completely.
    
    Change-Id: I8df39666366e8579acc9151988881ecef3c8f1a4
    Signed-off-by: Sushmita Susheelendra <quic_ssusheel@quicinc.com>
    Signed-off-by: Deepak Kumar <quic_dkumar@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched_hfi.c
@@ -1196,12 +1196,17 @@ skipib:
 	 */
 	wmb();
 
+	/*
+	 * Put the profiling information in the user profiling buffer.
+	 * The gmu_core_regwrite below has a wmb() before the actual
+	 * register write to ensure any pending writes are complete
+	 * before the register write.
+	 */
+	adreno_profile_submit_time(&time);
+
 	/* Send interrupt to GMU to receive the message */
 	gmu_core_regwrite(KGSL_DEVICE(adreno_dev), A6XX_GMU_HOST2GMU_INTR_SET,
 		DISPQ_IRQ_BIT(drawobj->context->gmu_dispatch_queue));
-
-	/* Put the profiling information in the user profiling buffer */
-	adreno_profile_submit_time(&time);
 
 	return ret;
 }
