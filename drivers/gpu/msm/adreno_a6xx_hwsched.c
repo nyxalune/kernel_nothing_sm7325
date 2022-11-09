45b25756e57de4cf94d45af835836d403645480f
Pankaj Gupta <quic_gpankaj@quicinc.com>
2022-11-09 09:07:17 +0530

    msm: kgsl: Enable BCL after fist boot complete for a6xx hwsched targets
    BCL needs respective Central Broadcast register to be programmed from
    TZ. This programming happens only when zap shader firmware load is
    successful. Zap firmware load can fail in boot up path hence enable
    BCL only after we successfully complete first boot to ensure that
    Central Broadcast register was programed before enabling BCL.
    
    Change-Id: I78c885891975a335c0862b1802383ed75e05cc91
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_a6xx_hwsched.c
+++ drivers/gpu/msm/adreno_a6xx_hwsched.c
@@ -584,6 +584,17 @@ static int a6xx_hwsched_first_boot(struct adreno_device *adreno_dev)
 	set_bit(GMU_PRIV_FIRST_BOOT_DONE, &gmu->flags);
 	set_bit(GMU_PRIV_GPU_STARTED, &gmu->flags);
 
+	/*
+	 * BCL needs respective Central Broadcast register to
+	 * be programed from TZ. This programing happens only
+	 * when zap shader firmware load is successful. Zap firmware
+	 * load can fail in boot up path hence enable BCL only after we
+	 * successfully complete first boot to ensure that Central
+	 * Broadcast register was programed before enabling BCL.
+	 */
+	if (ADRENO_FEATURE(adreno_dev, ADRENO_BCL))
+		adreno_dev->bcl_enabled = true;
+
 	device->pwrctrl.last_stat_updated = ktime_get();
 	device->state = KGSL_STATE_ACTIVE;
 
