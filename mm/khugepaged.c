43527725a7a0b4b3406591ad9705026f65e0a22b
Kazuki Hashimoto <kazukih0205@gmail.com>
2023-07-05 19:50:16 +0900

    Revert "mm/khugepaged: fix GUP-fast interaction by sending IPI"
    This reverts commit 48b00ceb5472c073a8101697d96cff8e4014fea2.
    
    Signed-off-by: Kazuki Hashimoto <kazukih0205@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1060,7 +1060,6 @@ static void collapse_huge_page(struct mm_struct *mm,
 	_pmd = pmdp_collapse_flush(vma, address, pmd);
 	spin_unlock(pmd_ptl);
 	mmu_notifier_invalidate_range_end(&range);
-	tlb_remove_table_sync_one();
 
 	spin_lock(pte_ptl);
 	isolated = __collapse_huge_page_isolate(vma, address, pte);
@@ -1408,7 +1407,6 @@ void collapse_pte_mapped_thp(struct mm_struct *mm, unsigned long addr)
 	mmu_notifier_invalidate_range_start(&range);
 	_pmd = pmdp_collapse_flush(vma, haddr, pmd);
 	mm_dec_nr_ptes(mm);
-	tlb_remove_table_sync_one();
 	mmu_notifier_invalidate_range_end(&range);
 	pte_free(mm, pmd_pgtable(_pmd));
 
@@ -1518,7 +1516,6 @@ static void retract_page_tables(struct address_space *mapping, pgoff_t pgoff)
 				/* assume page table is clear */
 				_pmd = pmdp_collapse_flush(vma, addr, pmd);
 				mm_dec_nr_ptes(mm);
-				tlb_remove_table_sync_one();
 				pte_free(mm, pmd_pgtable(_pmd));
 				mmu_notifier_invalidate_range_end(&range);
 			}
