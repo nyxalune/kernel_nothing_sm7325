269abd13ac61601796edaa3ec0c9f1452c218f3e
Alexander Winkowski <dereference23@outlook.com>
2024-03-10 05:14:00 +0000

    firmware_loader: Add /vendor/firmware
    From veux-r-oss
    
    Change-Id: I896e3908ad2ae1bb83461d0320b319b8ccc20bb0
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    [Tashar02: Change guard to CONFIG_MACH_NOTHING]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -457,9 +457,9 @@ static const char * const fw_path[] = {
 	fw_path_para[7],
 	fw_path_para[8],
 	fw_path_para[9],
+#ifdef CONFIG_MACH_NOTHING
 	"/vendor/firmware",
-	"/vendor/firmware_mnt/image",
-	"/firmware/image",
+#endif
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
