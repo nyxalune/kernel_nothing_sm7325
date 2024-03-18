c24b54f76b641b84fd54d460bb3b13f4593f217f
Pankaj Gupta <quic_gpankaj@quicinc.com>
2024-03-18 20:02:05 +0530

    msm: kgsl: Clear fault while booting GMU
    There are some instances where another fault is reported while initial
    fault was being handled. In such cases hwsched will trigger a new
    recovery as soon as the device_mutex gets released when the first
    recovery completes. This happens because fault bit remains set if we get
    another fault during the first recovery. Fix this problem by clearing
    hwsched fault while booting GMU.
    Also clear the fault (hwsched or dispatcher) before starting the GMU so
    any failure during GMU boot can be captured.
    
    Change-Id: I4b375217559d3fa640b83bc5b29042839f66eebe
    Signed-off-by: Pankaj Gupta <quic_gpankaj@quicinc.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/gpu/msm/adreno_hwsched.c
+++ drivers/gpu/msm/adreno_hwsched.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "adreno.h"
@@ -1314,6 +1314,14 @@ static void reset_and_snapshot(struct adreno_device *adreno_dev)
 done:
 	a6xx_hwsched_restart(adreno_dev);
 	adreno_hwsched_init_replay(hwsched);
+}
+
+void adreno_hwsched_clear_fault(struct adreno_device *adreno_dev)
+{
+	atomic_set(&adreno_dev->hwsched.fault, 0);
+
+	/* make sure other CPUs see the update */
+	smp_wmb();
 }
 
 static void adreno_hwsched_recovery(struct adreno_device *adreno_dev)
