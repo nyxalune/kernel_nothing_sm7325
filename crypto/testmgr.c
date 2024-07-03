caa7bccec741c5d897cb23eca5d8131b1d9a7f2d
Eric Biggers <ebiggers@google.com>
2024-07-03 12:04:31 -0700

    crypto: testmgr - generate power-of-2 lengths more often
    Implementations of hash functions often have special cases when lengths
    are a multiple of the hash function's internal block size (e.g. 64 for
    SHA-256, 128 for SHA-512).  Currently, when the fuzz testing code
    generates lengths, it doesn't prefer any length mod 64 over any other.
    This limits the coverage of these special cases.
    
    Therefore, this patch updates the fuzz testing code to generate
    power-of-2 lengths and divide messages exactly in half a bit more often.
    
    Reviewed-by: Sami Tolvanen <samitolvanen@google.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    (cherry picked from commit 101e99c23af9460890edc9055141e6aba6d3e08a)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>
    (cherry picked from commit bc4f6e3ee665148883da6eeed22d07bbffff1be3)
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -749,14 +749,20 @@ static unsigned int generate_random_length(unsigned int max_len)
 
 	switch (prandom_u32() % 4) {
 	case 0:
-		return len % 64;
+		len %= 64;
+		break;
 	case 1:
-		return len % 256;
+		len %= 256;
+		break;
 	case 2:
-		return len % 1024;
+		len %= 1024;
+		break;
 	default:
-		return len;
+		break;
 	}
+	if (len && prandom_u32_below(rng, 4) == 0)
+		len = rounddown_pow_of_two(len);
+	return len;
 }
 
 /* Sometimes make some random changes to the given data buffer */
@@ -839,6 +845,8 @@ static char *generate_random_sgl_divisions(struct test_sg_division *divs,
 
 		if (div == &divs[max_divs - 1] || prandom_u32() % 2 == 0)
 			this_len = remaining;
+		else if (prandom_u32_below(rng, 4) == 0)
+			this_len = (remaining + 1) / 2;
 		else
 			this_len = 1 + (prandom_u32() % remaining);
 		div->proportion_of_total = this_len;
