66b0ae3f650353a33c9fc717a6f1e848c9b94eda
Madara273 <ravenhoxs@gmail.com>
2024-09-14 15:57:26 -0700

    LRNG: Add support Linux Random Number Generator, ref: http://www.chronox.de/lrng
    ref: https://github.com/smuellerDD/lrng
    ref: https://www.chronox.de/lrng/
    
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6,10 +6,6 @@
  *
  *  Copyright (C) 1991-2002  Linus Torvalds
  */
-#include <linux/highmem.h>
-#include <linux/hrtimer_api.h>
-#include <linux/ktime_api.h>
-
 #include "sched.h"
 
 #include <linux/nospec.h>
