ec54b380654d4e43cdff3e7550c443bb205f4b06
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-01-12 23:26:40 +0600

    PM / devfreq: memlat: Fix division by zero in stall_pct calculation
    UBSAN warns of a division by zero for stall_pct in update_counts():
    
    [    1.716738] ================================================================================
    [    1.716752] UBSAN: Undefined behaviour in ../drivers/devfreq/arm-memlat-mon.c:198:25
    [    1.716756] division by zero
    [    1.716761] CPU: 3 PID: 68 Comm: kworker/u16:1 Not tainted 5.4.278-Scarlet-v1.0-beta8 #1
    [    1.716764] Hardware name: redwood based Qualcomm Technologies, Inc. SM7325 (DT)
    [    1.716775] Workqueue: memlat_wq memlat_monitor_work
    [    1.716777] Call trace:
    [    1.716783] dump_backtrace+0x0/0x230
    [    1.716786] show_stack+0x14/0x20
    [    1.716791] dump_stack+0x8c/0xcc
    [    1.716796] __ubsan_handle_divrem_overflow+0x1a4/0x1d0
    [    1.716798] memlat_monitor_work+0x51c/0x8a0
    [    1.716803] process_one_work+0x1c4/0x310
    [    1.716805] worker_thread+0x2ac/0x600
    [    1.716808] kthread+0x174/0x1b0
    [    1.716811] ret_from_fork+0x10/0x18
    [    1.716812] ================================================================================
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -205,14 +205,16 @@ static void update_counts(struct memlat_cpu_grp *cpu_grp)
 			cpu_grp->read_event_cpu = -1;
 		}
 
+		cpu_data->freq = common_evs[CYC_IDX].last_delta / delta;
+
 		if (!common_evs[STALL_IDX].pevent)
 			common_evs[STALL_IDX].last_delta =
 				common_evs[CYC_IDX].last_delta;
-
-		cpu_data->freq = common_evs[CYC_IDX].last_delta / delta;
-		cpu_data->stall_pct = mult_frac(100,
-				common_evs[STALL_IDX].last_delta,
-				common_evs[CYC_IDX].last_delta);
+		else
+			cpu_data->stall_pct = common_evs[CYC_IDX].last_delta ?
+				mult_frac(100, common_evs[STALL_IDX].last_delta,
+					  common_evs[CYC_IDX].last_delta) :
+				common_evs[STALL_IDX].last_delta;
 	}
 
 	for (i = 0; i < cpu_grp->num_mons; i++) {
