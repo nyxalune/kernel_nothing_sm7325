6cb99a7e68cc61407695b879add971c07f4be5e6
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-10-19 14:02:08 +0600

    zram: Increase zRAM size to 5GB
    After a prolonged uptime, e.g. 11 days, due to huge amount of memory leaks caused
    by apps like Telegram, zRAM is seen at 100% usage which showed severe amount of
    system slowdown. Increase 1GB more and make it 5GB. It is what it is.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2407,7 +2407,7 @@ static ssize_t disksize_store(struct device *dev,
 	int err;
 	u32 prio;
 
-	disksize = PAGE_ALIGN((u64)SZ_4G);
+	disksize = PAGE_ALIGN((u64)SZ_1G * 5);
 	pr_info("Setting zRAM size to %li GB", disksize / 1073741824);
 
 	down_write(&zram->init_lock);
