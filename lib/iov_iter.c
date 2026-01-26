9441a28927847807e4f542a80840e003ec35fcce
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-01-26 19:27:19 +0600

    iov_iter: Fix declaration after statement in pipe_zero()
    This fixes the following warning by Clang:
    
    ../lib/iov_iter.c:931:9: warning: mixing declarations and code is incompatible with standards before C99 [-Wdeclaration-after-statement]
      931 |                 char *p = kmap_local_page(pipe->bufs[idx].page);
          |                       ^
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- lib/iov_iter.c
+++ lib/iov_iter.c
@@ -1097,8 +1097,10 @@ static size_t pipe_zero(size_t bytes, struct iov_iter *i)
 
 	for ( ; n; idx = next_idx(idx, pipe), off = 0) {
 		size_t chunk = min_t(size_t, n, PAGE_SIZE - off);
+		char *p;
+
 		memzero_page(pipe->bufs[idx].page, off, chunk);
-		char *p = kmap_local_page(pipe->bufs[idx].page);
+		p = kmap_local_page(pipe->bufs[idx].page);
 		memset(p + off, 0, chunk);
 		kunmap_local(p);
 		i->idx = idx;
