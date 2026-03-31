1001195fcf8d8d2df6001eccbb7c5c0c9754859d
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-04-01 01:39:43 +0600

    Revert "fuse: use freezable wait in fuse_get_req()"
    We don't actually have a hang while freezing tasks from fuse_get_req(). So, for now
    we don't need this. Because with the commit, we use freezable wait and freezable wait
    utilizes try_to_freeze_unsafe() which exposes a rare but a big lock holding issue.
    
    With lockdep enabled, we see the following splat:
    
    [   38.728801]
    [   38.728807] ====================================
    [   38.728810] WARNING: busybox/1682 still has locks held!
    [   38.728813] 5.4.295-Scarlet-v4.2-beta1-@788dd252 #1 Tainted: G        W
    [   38.728816] ------------------------------------
    [   38.728819] 2 locks held by busybox/1682:
    [   38.728821] #0: ffffffad86d78140 (&type->i_mutex_dir_key#9){....}, at: lookup_slow+0x28/0x60
    [   38.728836] #1: ffffffad86d784c8 (&fi->mutex){....}, at: fuse_lock_inode+0x2c/0x40
    [   38.728844]
    stack backtrace:
    [   38.728847] CPU: 7 PID: 1682 Comm: busybox Tainted: G        W         5.4.295-Scarlet-v4.2-beta1-@788dd252 #1
    [   38.728849] Hardware name: redwood based Qualcomm Technologies, Inc. SM7325 (DT)
    [   38.728850] Call trace:
    [   38.728856] dump_backtrace+0x0/0x1a0
    [   38.728858] show_stack+0x14/0x20
    [   38.728863] __dump_stack+0x20/0x30
    [   38.728865] dump_stack+0x70/0x9c
    [   38.728869] print_held_locks_bug+0x84/0x90
    [   38.728871] debug_check_no_locks_held+0x20/0x30
    [   38.728874] fuse_get_req+0x1a4/0x3d0
    [   38.728876] fuse_simple_request+0x120/0x720
    [   38.728878] fuse_lookup_name+0x110/0x270
    [   38.728880] fuse_lookup+0x70/0x1b0
    [   38.728882] __lookup_slow+0x108/0x1a0
    [   38.728884] lookup_slow+0x38/0x60
    [   38.728886] walk_component+0xd4/0x130
    [   38.728888] link_path_walk+0x25c/0x4d0
    [   38.728891] path_lookupat+0x2c/0x190
    [   38.728892] filename_lookup+0xc4/0x1e0
    [   38.728894] user_path_at_empty+0x44/0x60
    [   38.728896] vfs_statx+0x84/0x130
    [   38.728898] __arm64_sys_newfstatat+0x80/0xc0
    [   38.728900] el0_svc_common+0xb0/0x120
    [   38.728902] do_el0_svc+0x18/0x20
    [   38.728904] el0_sync_handler+0x13c/0x180
    [   38.728905] el0_sync+0x140/0x180
    
    Without the patch, the lock holding still exists. fuse_get_req() previously set
    the task state to TASK_KILLABLE. This is NOT freezable, so the freezer never
    tries to freeze the task while it’s waiting. No call to try_to_freeze_unsafe()
    -> No lockdep checks to see whether locks are held. It's a dormant bug. It could
    also be that holding the locks while freezing is intentional because in mainline
    there's TASK_FREEZABLE_UNSAFE to explicitly tells the freezer about this. But as
    of now, we don't know yet since mainline did not address this. It's better to not
    be oversmart.
    
    To silence the lockdep warning for now, revert this since we don't have a system
    hang right now anyways. If we get any hang from fuse_get_req(), we'll reapply it
    back again. But revert this for now until we come up with a better fix to properly
    fix the lockdep warning which shows an actual bug in FUSE.
    
    This reverts commit 3282e992fbfb1b4f1c7499daa66e5497eb335650.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -113,9 +113,8 @@ static struct fuse_req *fuse_get_req(struct fuse_mount *fm, bool for_background)
 
 	if (fuse_block_alloc(fc, for_background)) {
 		err = -EINTR;
-		if (wait_event_state_exclusive(fc->blocked_waitq,
-				!fuse_block_alloc(fc, for_background),
-				(TASK_KILLABLE | TASK_FREEZABLE)))
+		if (wait_event_killable_exclusive(fc->blocked_waitq,
+				!fuse_block_alloc(fc, for_background)))
 			goto out;
 	}
 	/* Matches smp_wmb() in fuse_set_initialized() */
