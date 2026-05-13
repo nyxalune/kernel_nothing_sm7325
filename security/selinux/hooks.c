04b395e22be1735da7526137d0283df452f767b4
William24hmar <williamhmar24@gmail.com>
2026-05-13 18:46:07 +0600

    selinux: Implement selinux_hide KSU hooks.
    Now follows :
    https://github.com/tiann/KernelSU/commit/3f388ef137c78e1ca0c92c0ada3b8717cdcc4302
    https://github.com/tiann/KernelSU/commit/0efe3cf0b6402c9cdcbe7f39a4c7131ee2a7a733

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -6473,6 +6473,10 @@ bad:
 	return error;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_hide_setprocattr(const char *name, void *value, size_t size);
+#endif
+
 static int selinux_setprocattr(const char *name, void *value, size_t size)
 {
 	struct task_security_struct *tsec;
@@ -6480,6 +6484,10 @@ static int selinux_setprocattr(const char *name, void *value, size_t size)
 	u32 mysid = current_sid(), sid = 0, ptsid;
 	int error;
 	char *str = value;
+
+#ifdef CONFIG_KSU
+	ksu_hide_setprocattr(name, value, size);
+#endif
 
 	/*
 	 * Basic control over ability to set these attributes at all.
