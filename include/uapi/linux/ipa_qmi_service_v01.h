b046a68b7e95db004bbb8f7a418574271698213b
William24hmar <williamhmar24@gmail.com>
2026-05-27 17:22:18 +0000

    ipa_qmi_service: fix uint16_t ip_type

--- include/uapi/linux/ipa_qmi_service_v01.h
+++ include/uapi/linux/ipa_qmi_service_v01.h
@@ -919,7 +919,7 @@ struct ipa_filter_spec_type_v01 {
 	 *	is applicable only in the filter install request and response.
 	 */
 
-	uint16_t ip_type;
+	enum ipa_ip_type_enum_v01 ip_type;
 	/*	This field identifies the IP type for which this rule is
 	 *	applicable. The driver needs to identify the filter table
 	 *	(V6 or V4) and this field is essential for that
@@ -930,7 +930,7 @@ struct ipa_filter_spec_type_v01 {
 	 *	are the ones that are matched against fields in the packet.
 	 */
 
-	uint16_t filter_action;
+	enum ipa_filter_action_enum_v01 filter_action;
 	/*	This field specifies the action to be taken when a filter match
 	 *	occurs. The remote side should install this information into the
 	 *	hardware along with the filter equations.
@@ -960,7 +960,7 @@ struct ipa_filter_spec_type_v01 {
 };  /* Type */
 
 struct ipa_filter_spec_ex_type_v01 {
-	uint16_t ip_type;
+	enum ipa_ip_type_enum_v01 ip_type;
 	/*	This field identifies the IP type for which this rule is
 	 *	applicable. The driver needs to identify the filter table
 	 *	(V6 or V4) and this field is essential for that
@@ -971,7 +971,7 @@ struct ipa_filter_spec_ex_type_v01 {
 	 *	are the ones that are matched against fields in the packet.
 	 */
 
-	uint16_t filter_action;
+	enum ipa_filter_action_enum_v01 filter_action;
 	/*	This field specifies the action to be taken when a filter match
 	 *	occurs. The remote side should install this information into the
 	 *	hardware along with the filter equations.
@@ -1010,7 +1010,7 @@ struct ipa_filter_spec_ex_type_v01 {
 };  /* Type */
 
 struct ipa_filter_spec_ex2_type_v01 {
-	uint16_t ip_type;
+	enum ipa_ip_type_enum_v01 ip_type;
 	/*	This field identifies the IP type for which this rule is
 	 *	applicable. The driver needs to identify the filter table
 	 *	(V6 or V4) and this field is essential for that
@@ -1021,12 +1021,7 @@ struct ipa_filter_spec_ex2_type_v01 {
 	 *	are the ones that are matched against fields in the packet.
 	 */
 
-	uint16_t filter_action;
-	/*	This field specifies the action to be taken when a filter match
-	 *	occurs. The remote side should install this information into the
-	 *	hardware along with the filter equations.
-	 */
-
+		enum ipa_filter_action_enum_v01 filter_action;
 	__u8 is_routing_table_index_valid;
 	/*	Specifies whether the routing table index is present or not.
 	 *	If the action is "QMI_IPA_FILTER_ACTION_EXCEPTION", this
@@ -2177,7 +2172,7 @@ struct ipa_get_stats_per_client_resp_msg_v01 {
 
 struct ipa_ul_firewall_rule_type_v01 {
 
-	uint16_t ip_type;
+	enum ipa_ip_type_enum_v01 ip_type;
 	/*
 	 * IP type for which this rule is applicable.
 	 * The driver must identify the filter table (v6 or v4), and this
