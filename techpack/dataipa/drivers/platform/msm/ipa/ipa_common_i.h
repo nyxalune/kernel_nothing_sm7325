9a3cd98ea59e324c442407fa5c9904fba207730d
William24hmar <williamhmar24@gmail.com>
2026-05-29 13:32:20 +0000

    Revert "techpack: dataipa: Fix conflicting function prototypes"
    This reverts commit 646effa6f1f6f22ecddebc8730b5e35ca5c78c3d.

--- techpack/dataipa/drivers/platform/msm/ipa/ipa_common_i.h
+++ techpack/dataipa/drivers/platform/msm/ipa/ipa_common_i.h
@@ -601,10 +601,9 @@ int ipa_wigig_send_wlan_msg(enum ipa_wlan_event msg_type,
 	const char *netdev_name, u8 *mac);
 
 void ipa3_register_client_callback(int (*client_cb)(bool is_lock),
-			bool (*teth_port_state)(void),
-			enum ipa_client_type client_type);
+			bool (*teth_port_state)(void), u32 ipa_ep_idx);
 
-void ipa3_deregister_client_callback(enum ipa_client_type client_type);
+void ipa3_deregister_client_callback(u32 ipa_ep_idx);
 
 /*
 * Configuration
